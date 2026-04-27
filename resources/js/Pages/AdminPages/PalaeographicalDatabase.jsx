99d64f5bc9be9bd618583d053a588a5a30ea6609
Udaya Adhikari <adhikariudaya521@gmail.com>
2026-04-27 10:41:43 +0545

    Makes Changes on the  few pages

--- resources/js/Pages/AdminPages/PalaeographicalDatabase.jsx
+++ resources/js/Pages/AdminPages/PalaeographicalDatabase.jsx
@@ -224,7 +224,6 @@ const PalaeographicalDatabase = () => {
                 accessor: "period",
                 Cell: ({ row }) => <span>{row.original.period || "—"}</span>,
             },
-            
             {
                 Header: "Script",
                 accessor: "script",
