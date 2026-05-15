bcc9a3a018ed359ead398d2f962d11c17d116e7b
Udaya Adhikari <adhikariudaya521@gmail.com>
2026-05-15 12:11:26 +0545

    Add the search filter

--- routes/api.php
+++ routes/api.php
@@ -1,50 +1,48 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ChildCategoryController;
 use App\Http\Controllers\InscriptionController;
+use App\Http\Controllers\PalaeographicalController;
+use App\Http\Controllers\SubCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\SubCategoryController;
-use App\Http\Controllers\PalaeographicalController;
-use App\Http\Controllers\ChildCategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-    // -----------------------------------------
-    // Inscription API Routes
-    // -----------------------------------------
+// -----------------------------------------
+// Inscription API Routes
+// -----------------------------------------
 
-    Route::get('/inscriptions', [InscriptionController::class, 'index']);
+Route::get('/inscriptions', [InscriptionController::class, 'index']);
 
+// -----------------------------------------
+// Inscription Details by Slug
+// -----------------------------------------
 
+Route::get('/{slug}/details', [InscriptionController::class, 'showBySlug']);
 
-    // -----------------------------------------
-    // Inscription Details by Slug
-    // -----------------------------------------
+// -----------------------------------------
+// Inscription Search Route
+// -----------------------------------------
 
-    Route::get('/{slug}/details', [InscriptionController::class, 'showBySlug']);
-    
-    // -----------------------------------------
-    // Inscription Search Route
-    // -----------------------------------------
+Route::get('/palaeographical', [PalaeographicalController::class, 'index']);
 
-    Route::get('/palaeographical', [PalaeographicalController ::class, 'index']);
-  
-    // -----------------------------------------
-    // SubCategory API Routes
-    // -----------------------------------------
+// -----------------------------------------
+// SubCategory API Routes
+// -----------------------------------------
 
-    Route::get('/sub_categories', [SubCategoryController::class, 'index']);
+Route::get('/sub_categories', [SubCategoryController::class, 'index']);
 
-    // -----------------------------------------
-    // Category API Routes
-    // -----------------------------------------
+// -----------------------------------------
+// Category API Routes
+// -----------------------------------------
 
-    Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
 
-    // -----------------------------------------
-    // Child Category API Routes
-    // -----------------------------------------
-    Route::get('/child_categories', [ChildCategoryController ::class, 'index']);
+// -----------------------------------------
+// Child Category API Routes
+// -----------------------------------------
+Route::get('/child_categories', [ChildCategoryController::class, 'index']);
